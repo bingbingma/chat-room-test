a5165087831a5bec93d86f4fd32611825d7cb489
bingbingma <7047982+bingbingma@users.noreply.github.com>
2019-04-25 21:12:05 -0400

    socket.io example

---
+++ index.js
@@ -0,0 +1,42 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+// app.get("/", function(req, res) {
+//   res.send("<h1>Hello world</h1>");
+// });
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  console.log("an user connected");
+});
+
+io.on("connection", function(socket) {
+  console.log("an user connected");
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+});
+
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    console.log("message: " + msg);
+  });
+});
+
+io.on("connection", function(socket) {
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+  });
+});
+
+io.emit("some event", { for: "everyone" });
+
+http.listen(3000, function() {
+  console.log(`listening on http://localhost:3000`);
+});
+
+///////
